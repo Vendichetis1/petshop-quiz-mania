c5932a5c6184fb6ce9309eeb48de31d57eae882a
Vendichetis1 <valentimvendicletis@gmail.com>
2025-05-24 10:26:19 -0300

    alterei o cachorrin

--- src/components/PetMascot.tsx
+++ src/components/PetMascot.tsx
@@ -16,47 +16,51 @@ const PetMascot: React.FC<PetMascotProps> = ({ isHappy = false, size = 'medium' 
     <div className={`${sizeClasses[size]} relative mx-auto`}>
       <div
         className={`
-          relative w-full h-full bg-amber-300 rounded-[45%]
-          transition-transform duration-300 shadow-lg
+          relative w-full h-full bg-amber-300 rounded-[40%] shadow-lg
+          transition-transform duration-300
           ${isHappy ? 'animate-bounce scale-110' : 'hover:scale-105'}
         `}
       >
         {/* Ears - floppy dog style */}
-        <div className="absolute -top-3 -left-3 w-6 h-10 bg-amber-600 rounded-full transform rotate-[-30deg]"></div>
-        <div className="absolute -top-3 -right-3 w-6 h-10 bg-amber-600 rounded-full transform rotate-[30deg]"></div>
+        <div className="absolute -top-2 -left-4 w-6 h-14 bg-amber-600 rounded-full rotate-[-30deg]"></div>
+        <div className="absolute -top-2 -right-4 w-6 h-14 bg-amber-600 rounded-full rotate-[30deg]"></div>
 
-        {/* Eyes - bigger and expressive */}
-        <div className="absolute top-6 left-6 w-4 h-4 bg-black rounded-full">
-          <div className="w-1.5 h-1.5 bg-white rounded-full mt-0.5 ml-0.5"></div>
+        {/* Eyes */}
+        <div className="absolute top-7 left-5 w-5 h-5 bg-black rounded-full shadow-sm">
+          <div className="w-2 h-2 bg-white rounded-full mt-0.5 ml-0.5"></div>
         </div>
-        <div className="absolute top-6 right-6 w-4 h-4 bg-black rounded-full">
-          <div className="w-1.5 h-1.5 bg-white rounded-full mt-0.5 ml-0.5"></div>
+        <div className="absolute top-7 right-5 w-5 h-5 bg-black rounded-full shadow-sm">
+          <div className="w-2 h-2 bg-white rounded-full mt-0.5 ml-0.5"></div>
         </div>
 
-        {/* Nose - triangular */}
-        <div className="absolute top-[48%] left-1/2 transform -translate-x-1/2 w-3 h-2 bg-black rounded-t-full"></div>
+        {/* Nose */}
+        <div className="absolute top-[52%] left-1/2 transform -translate-x-1/2 w-3 h-3 bg-black rounded-t-full shadow-sm"></div>
 
         {/* Mouth & tongue */}
         {isHappy ? (
           <>
-            <div className="absolute top-[62%] left-1/2 transform -translate-x-1/2 w-5 h-3 border-b-2 border-black rounded-b-full"></div>
-            <div className="absolute top-[66%] left-1/2 transform -translate-x-1/2 w-2.5 h-2 bg-pink-500 rounded-full"></div>
+            <div className="absolute top-[65%] left-1/2 transform -translate-x-1/2 w-6 h-4 border-b-2 border-black rounded-b-full"></div>
+            <div className="absolute top-[70%] left-1/2 transform -translate-x-1/2 w-3 h-2 bg-pink-500 rounded-full"></div>
           </>
         ) : (
-          <div className="absolute top-[62%] left-1/2 transform -translate-x-1/2 w-5 h-[2px] bg-black rounded-full"></div>
+          <div className="absolute top-[65%] left-1/2 transform -translate-x-1/2 w-6 h-[2px] bg-black rounded-full"></div>
         )}
 
         {/* Cheeks */}
-        <div className="absolute top-[65%] left-4 w-2.5 h-2.5 bg-pink-300 rounded-full opacity-70"></div>
-        <div className="absolute top-[65%] right-4 w-2.5 h-2.5 bg-pink-300 rounded-full opacity-70"></div>
+        <div className="absolute top-[68%] left-4 w-2.5 h-2.5 bg-pink-300 rounded-full opacity-70"></div>
+        <div className="absolute top-[68%] right-4 w-2.5 h-2.5 bg-pink-300 rounded-full opacity-70"></div>
 
-        {/* Tail - more fluffy dog tail style */}
+        {/* Tail - fluffy curve */}
         <div
           className={`
-            absolute -right-4 top-[30%] w-3 h-10 bg-amber-600 rounded-full origin-bottom-right
-            ${isHappy ? 'animate-tail-wag' : 'rotate-[20deg]'}
+            absolute -right-5 top-[30%] w-3 h-12 bg-amber-600 rounded-full origin-bottom-right
+            ${isHappy ? 'animate-tail-wag' : 'rotate-[25deg]'}
           `}
         ></div>
+
+        {/* Paws */}
+        <div className="absolute bottom-1 left-6 w-3 h-3 bg-amber-500 rounded-full shadow-sm"></div>
+        <div className="absolute bottom-1 right-6 w-3 h-3 bg-amber-500 rounded-full shadow-sm"></div>
       </div>
 
       {/* Sparkles when happy */}
@@ -69,8 +73,8 @@ const PetMascot: React.FC<PetMascotProps> = ({ isHappy = false, size = 'medium' 
       {/* Custom Animations */}
       <style>{`
         @keyframes tail-wag {
-          0%, 100% { transform: rotate(20deg); }
-          50% { transform: rotate(-20deg); }
+          0%, 100% { transform: rotate(25deg); }
+          50% { transform: rotate(-15deg); }
         }
         .animate-tail-wag {
           animation: tail-wag 0.4s infinite ease-in-out;
